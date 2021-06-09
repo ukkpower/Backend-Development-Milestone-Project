dc6845771706d7bdc6e1a91ca41bbd76610d7199
ukkpower <ukkpower@gmail.com>
2021-06-09 20:32:35 +0000

    added: navbar flask routing

--- run.py
+++ run.py
@@ -10,6 +10,26 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/public")
+def public():
+    return render_template("public.html")
+
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
