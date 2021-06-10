3168c297dc004042425f8e4b8d690d8e09e1bda0
ukkpower <ukkpower@gmail.com>
2021-06-10 23:06:56 +0000

    update: signup form now registers into the database

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -24,7 +25,7 @@ def index():
 
 @app.route("/public")
 def public():
-    polls = mongo.db.tasks.find()
+    polls = mongo.db.polls.find()
     return render_template("public.html", polls=polls)
 
 
@@ -38,8 +39,30 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "firstName": request.form.get("firstName"),
+            "lastName": request.form.get("lastName"),
+            "email": request.form.get("email").lower(),
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+
     return render_template("signup.html")
 
 
