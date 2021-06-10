85e06c90da306ebcd65b3fe0dd0355525fed4103
ukkpower <ukkpower@gmail.com>
2021-06-10 21:26:46 +0000

    update: retreived records and diaplyed in public.html

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import os
-from flask import Flask, render_template, request, flash
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route("/")
@@ -15,7 +24,8 @@ def index():
 
 @app.route("/public")
 def public():
-    return render_template("public.html")
+    polls = mongo.db.tasks.find()
+    return render_template("public.html", polls=polls)
 
 
 @app.route("/new")
